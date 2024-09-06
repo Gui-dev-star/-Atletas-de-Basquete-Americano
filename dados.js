70458a44529b7c7c7796b6fe1493f4f420035781
Gui-dev-star <guilhermeeller49@gmail.com>
2024-09-06 17:02:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+  {
+    titulo: "Lebron James",
+    descricao: "Lebron James é um dos maiores jogadores de basquete de todos os tempos. Ele é conhecido por sua incrível habilidade de jogar, sua força e sua altura.",
+    link: "https://pt.wikipedia.org/wiki/Lebron_James",
+    titulos: 4, 
+    timeAtual: "Los Angeles Lakers",
+    tags: "King James Rei Basketball Basquete"
+    
+  },
+  {
+    titulo: "Kobe Bryant",
+    descricao: "Kobe Bryant foi um dos maiores jogadores de basquete de todos os tempos. Conhecido por sua ética de trabalho e habilidade de arremesso, ele conquistou cinco campeonatos.",
+    link: "https://pt.wikipedia.org/wiki/Kobe_Bryant",
+    titulos: 5,
+    timeAtual: "Aposentado",
+    tags: "Mamba Mamba negra Basketball Basquete"
+  },
+  {
+    titulo: "Trae Young",
+    descricao: "Trae Young é um jovem talentoso da NBA, conhecido por suas habilidades de arremesso de longa distância e visão de jogo. Ele é considerado um dos futuros astros da NBA.",
+    link: "https://pt.wikipedia.org/wiki/Trae_Young",
+    titulos: 0,
+    timeAtual: "Atlanta Hawks",
+    tags: " O rei das canetas Cold Basketball Basquete "
+  }
+];
